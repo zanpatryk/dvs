f42779787e15d555c99fe3eec62ff15c24c810a7
YouRekt <czarnecki.jurko@gmail.com>
2025-03-31 22:32:21 +0200

    feat: Update App component layout with Navbar and RainbowButton for enhanced user experience

--- dvs-app/src/App.tsx
+++ dvs-app/src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RainbowButton } from "@/components/ui/rainbow-button";
+import Navbar from "@/LandingPage/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<div className="relative overflow-hidden h-svh">
+				<Navbar />
+				<div className="flex flex-col items-center justify-center space-y-20">
+					<h1 className="text-8xl font-bold">Welcome</h1>
+					<h1 className="text-5xl font-bold">to the first</h1>
+					<div className="text-8xl font-bold">
+						<span className="text-transparent bg-clip-text bg-linear-to-r/increasing from-indigo-500 to-teal-400">
+							Decentralized Voting System
+						</span>
+					</div>
+				</div>
+				<div className="flex flex-col items-center justify-center mt-96 space-y-8">
+					<RainbowButton>Get Started</RainbowButton>
+				</div>
+				<img
+					src="/blob1.png"
+					className="absolute -top-100 -right-100 w-1/2 opacity-75 -z-1"
+					alt="Background Blob"
+				/>
+				<img
+					src="/blob2.png"
+					className="absolute -bottom-300 -left-100 w-1/2 opacity-75 -z-1"
+					alt="Background Blob"
+				/>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
