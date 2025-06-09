bb0274a7ea067c064cbbcaf7a076a6a8f4b2b7fe
YouRekt <czarnecki.jurko@gmail.com>
2025-06-09 22:20:05 +0200

    refactor: remove unused client, contract hooks, and dashboard routes

--- dvs-app/src/hono-client.ts
+++
@@ -1,8 +0,0 @@
-import { hc } from "hono/client";
-import { type ApiRoutes } from "../../backend/src/index";
-
-export const client = hc<ApiRoutes>("/", {
-	init: {
-		credentials: "include",
-	},
-});
